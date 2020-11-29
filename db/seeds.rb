626ca1b31a72cad46dc64a9ce5045d0b4599d6f9
creattech <crystalsmcneil@gmail.com>
2020-11-28 21:38:13 -0600

    assigns user to course

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,5 @@
+User.create!(email: 'admin@creativarian.com', password: 'Test123456', password_confirmation: 'Test123456')
+
 30.times do
-  Course.create!([{
-                      title: Faker::Educator.course_name,
-                      description: Faker::TvShows::GameOfThrones.quote
-                  }])
+  Course.create!([{title: Faker::Educator.course_name, description: Faker::TvShows::GameOfThrones.quote,user_id: User.first.id }])
 end
